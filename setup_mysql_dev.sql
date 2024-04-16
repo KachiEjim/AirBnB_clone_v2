4992cb55fc1d23d63a1aa11be6fc55099bcb4ad3
Chabang28 <chabangmnisi@gmail.com>
2024-04-16 09:36:27 -0700

    tests

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,13 @@
 -- prepares a MySQL server for the project.
+
+-- Create database if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user if it doesn't already exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@localhost';
+
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev_db. * TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
+GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@localhost';
