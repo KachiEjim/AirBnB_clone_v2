ba759edb4960537c74aec2e9875250cf86a659a3
ejimvincent <ejimovc@gmail.com>
2024-04-16 21:14:42 +0100

    Merging branches to master

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -10,4 +10,4 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db. * TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
-GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev' @'localhost';
