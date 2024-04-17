d8fec52e270a1849eafcf186bb1266c534c2e073
ejimvincent <ejimovc@gmail.com>
2024-04-17 22:24:24 +0100

    Added Dbstorage class

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 from os import getenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from models.base_model import BaseModel, Base
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-
-
 
 class DBStorage:
     """Defines DB storage class"""
@@ -18,6 +7,17 @@ class DBStorage:
     __session = None
 
     def __init__(self):
+        """Initializes the DBstorage class"""
+
+        from sqlalchemy import create_engine
+        from models.base_model import Base
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.place import Place
+        from models.amenity import Amenity
+        from models.review import Review
+
         mysql_user = getenv('HBNB_MYSQL_USER')
         mysql_pwd = getenv('HBNB_MYSQL_PWD')
         mysql_host = getenv('HBNB_MYSQL_HOST', default='localhost')
@@ -32,6 +32,15 @@ class DBStorage:
     def all(self, cls=None):
         """query on the current database session (self.__session) all objects
         depending of the class name (argument cls)"""
+
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.place import Place
+        from models.amenity import Amenity
+        from models.review import Review
+
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -67,13 +76,15 @@ class DBStorage:
             self.__session.delete(obj)
 
     def reload(self):
-        """"""
+        """Reloads objects from the database"""
+
+        from sqlalchemy.orm import sessionmaker, scoped_session
+        from models.base_model import Base
+
         Base.metadata.create_all(self.__engine)
         Session_maker = sessionmaker(bind=self.__engine,
                                      expire_on_commit=False)
         self.__session = scoped_session(Session_maker)()
-
-
 
     def close(self):
         """closes the working SQLAlchemy session"""
