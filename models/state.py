4497a3e43dfcd67d76d9d3aae6f5e2688778c7d2
ejimvincent <ejimovc@gmail.com>
2024-04-17 12:44:29 +0100

    Update to state and city

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+
+    @property
+    def cities(self):
+        """getter attribute cities that returns
+        the list of City instances with
+        state_id equals to the current State.id"""
+        result = []
+        from models import storage
+        cities = storage.all(City)
+        for city in cities.values():
+            if city.state_id == self.id:
+                result.append(city)
+        return result
