4b1093d0f7b929f3a4447e34436cbddd2d1a18a2
ejimvincent <ejimovc@gmail.com>
2024-04-20 20:58:29 +0100

    Completeing task 6 and 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -39,7 +39,7 @@ class DBStorage:
         depending of the class name (argument cls)"""
         objs_dict = {}
         if cls is None:
-            tables_list = [State, City]
+            tables_list = [State, City, User, Amenity, Place, Review]
 
         else:
             if isinstance(cls, str):
