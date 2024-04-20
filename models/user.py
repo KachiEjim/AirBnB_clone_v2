9abf6f2bd9f74f691abc1132e442e769986f96dc
ejimvincent <ejimovc@gmail.com>
2024-04-20 21:07:26 +0100

    Completeing task 6 and 7

--- models/user.py
+++ models/user.py
@@ -8,8 +8,15 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """Representation of a user """
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
+    email = Column(String(128),
+                   nullable=False)
+    password = Column(String(128),
+                      nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship('Place', cascade='all, delete', backref='user')
+    places = relationship('Place',
+                          cascade='all, delete',
+                          backref='user')
+    reviews = relationship('Review',
+                           cascade='all, delete',
+                           backref='user')
