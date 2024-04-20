f2c49be4c32a8fbbff9dcc0e0d51135e1beb4016
ejimvincent <ejimovc@gmail.com>
2024-04-20 14:06:39 +0100

    Still debuging

--- models/base_model.py
+++ models/base_model.py
@@ -40,6 +40,10 @@ class BaseModel():
                 self.id = str(uuid.uuid4())
             self.__dict__.update(kwargs)
 
+            for key, value in kwargs.items():
+                if not hasattr(self, key):
+                    setattr(self, key, value)
+
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
