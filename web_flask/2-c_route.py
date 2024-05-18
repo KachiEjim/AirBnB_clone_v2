ab4999c81caca59f80cce503921ebb2e967e346c
ejimvincent <ejimovc@gmail.com>
2024-05-18 10:21:36 +0100

    BUILD(AirBnB_clone_v2:web_flask:Task 2)display “C ” followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application
+listening on 0.0.0.0, port 5000"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    This function is the home route of the Flask application.
+
+    Returns:
+        str: The string "Hello HBNB!" as the response to the home route.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    This function is the home route of the Flask application.
+
+    Returns:
+        str: The string "HBNB!" as the response to the hbnb route.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C " + text.replace("_", " ")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
