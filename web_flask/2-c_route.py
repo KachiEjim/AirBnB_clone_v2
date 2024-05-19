9253a7f337b176c671d9a4ddd77045086673fe0f
ejimvincent <ejimovc@gmail.com>
2024-05-19 10:54:59 +0100

    FIX(AirBnB_clone_v2:Task 8) Made pycodestlye happy

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -32,15 +32,18 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """
-        This function is the route for the '/c/<text>' endpoint of the Flask application.
+        This function is the route for the '/c/<text>'
+        endpoint of the Flask application.
 
         Parameters:
             text (str): The text parameter extracted from the URL.
 
         Returns:
-            str: The string "C " concatenated with the text parameter after replacing underscores with spaces.
+            str: The string "C " concatenated with the
+            text parameter after replacing underscores with spaces.
     """
     return "C " + text.replace("_", " ")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
