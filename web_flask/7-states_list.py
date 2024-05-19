8d3c55191b3a5455317fc78e3a31f303f95127c0
ejimvincent <ejimovc@gmail.com>
2024-05-19 06:51:31 +0100

    upload

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,13 +8,13 @@ from models import storage
 app = Flask(__name__)
 
 
-
 @app.route('/states_list', strict_slashes=False)
 def index():
     """ Route to display all objects from storage """
     states = storage.all("State").values()
     sorted_states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
