a0575e6488c440fc05521b9445de1165cc421c77
ejimvincent <ejimovc@gmail.com>
2024-05-20 12:35:00 +0100

    upload

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -62,7 +62,8 @@ def states_list():
     """ Renders a template that lists states
     retrieved from the storage. """
     states = storage.all(State)
-    list_states = sorted(states.values())
+    list_states = sorted(states.values()
+                         , key=lambda state: state.name)
     return render_template('7-states_list.html', states=list_states)
 
 
