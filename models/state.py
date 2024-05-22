357275e4e7c7624e71b372d406568df7c22e220d
ejimvincent <ejimovc@gmail.com>
2024-05-22 13:53:15 +0100

    BUILD(AirBnB_clone_v2:Task 10) starts a Flask web applicatio

--- models/state.py
+++ models/state.py
@@ -18,7 +18,6 @@ class State(BaseModel, Base):
         cities = relationship('City', cascade="all,delete", backref="state")
     else:
         name = ""
-
         @property
         def cities(self):
             """getter docuemnt"""
